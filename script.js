52fbffc47732a890962357e11dc6a0d659ec9f2e
Sitakant-Sarangi <sitakantsarangi1981@gmail.com>
2026-01-27 23:19:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let studentName ="John";
+let studentAge = 20;
+let isEnrolled = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+//string
+let firstName = "John";
+let middleName = 'Patric';
+let lastName = `Doe`;
+
+console.log("student's firstName is John middleName is 'Patric' and last name is `Doe`");
+
+//number
+let weight = 120.5;
+let height = 180;
+
+console.log(weight);
+console.log(height);
+
+//boolean
+
+let student = true;
+let teacher = false;
+
+console.log(student);
+console.log(teacher);
+
+//arrey
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[1]);
+fruits.push("orange");
+console.log(fruits[3]);
+console.log(fruits);
+
+//object
+
+const myObject = {firstName: "John", lastName: "Doe"};
+console.log(myObject);
+
+// arithmatic operators
+
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2/2);
+
+//comparison operators
+
+let a = 6;
+let b = 7;
+console.log(a < b);
+console.log(a > b);
+console.log(a === b);
+console.log(a !== b);
+
+//logical operators
+
+let c = true;
+let d = false;
+console.log(c && d);
+console.log(c || d);
+console.log(!c);
